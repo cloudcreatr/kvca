ab317ae435cd1c6ca76457144713e6d47e1dcd81
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2024-03-08 14:58:12 +0000

    Add performance logging and return additional data from loader function

--- app/routes/_index.ts
+++ app/routes/_index.ts
@@ -7,7 +7,26 @@ import {
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
   const kv = context.cloudflare.env.kvcache;
-  
+  const cf = context.cloudflare.cf;
+  const start = performance.now();
+  const data = await kv.get("om", { type: "json" });
+  const end = performance.now();
+  const timing = end - start; 
+  console.log("KV GET TIME: ", {
+    time: timing,
+    colo: cf.colo,
+    country: cf.country,
 
-  return await kv.get("om", { type: "json" })
-}
+  });
+  return {
+    data: data,
+    
+    time: timing,
+    colo: cf.colo,
+    country: cf.country,
+
+  
+  };
+
+  }
+
