ae77d976c8c02f58f265522f9c7a39fc6cda1f87
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2024-03-09 14:20:04 +0000

    Refactor imports and update cache key

--- app/routes/cdb.ts
+++ app/routes/cdb.ts
@@ -1,4 +1,9 @@
-import { ActionFunctionArgs, AppLoadContext, LoaderFunctionArgs, json } from "@remix-run/cloudflare";
+import {
+  ActionFunctionArgs,
+  AppLoadContext,
+  LoaderFunctionArgs,
+  json,
+} from "@remix-run/cloudflare";
 import { articles } from "drizzle/schema";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
@@ -7,37 +12,34 @@ function getDB(context: AppLoadContext) {
   const client = postgres(connectionString, {
     prepare: false,
   });
-  return drizzle(client);   
+  return drizzle(client);
 }
 
 interface InsertPayload {
-  type: "INSERT"  | "UPDATE" | "DELETE";
+  type: "INSERT" | "UPDATE" | "DELETE";
   table: "article";
   schema: string;
-  record: typeof articles
+  record: typeof articles;
   old_record: null;
 }
 
-
-
 export async function loader({ context }: LoaderFunctionArgs) {
   const kv = context.cloudflare.env.kvcache;
-  const { ctx } = context.cloudflare; 
-   const start = performance.now();
-  const data = await kv.get("articles", { type: "stream" });
+  const { ctx } = context.cloudflare;
+  const start = performance.now();
+  const data = await kv.get("om2", { type: "stream" });
   const end = performance.now();
   const time = end - start;
-  
-    if (data) {
-      return new Response(data, {
-        headers: {
-          "X-KV-Cache": "HIT",
-          "X-KV-Cache-Time": `${time}ms`,
-          "content-type": "application/json"
-          
-        },
-        });
-    }
+
+  if (data) {
+    return new Response(data, {
+      headers: {
+        "X-KV-Cache": "HIT",
+        "X-KV-Cache-Time": `${time}ms`,
+        "content-type": "application/json",
+      },
+    });
+  }
   const db = getDB(context);
   const start2 = performance.now();
   const data2 = await db.select().from(articles);
@@ -48,39 +50,33 @@ export async function loader({ context }: LoaderFunctionArgs) {
     time2,
   });
 
-  
-     await kv.put("articles", JSON.stringify(data2));
-  
+  await kv.put("articles", JSON.stringify(data2));
 
   return json(data2, {
     headers: {
       "X-KV-Cache": "MISS",
       "X-KV-Cache-Time": `${time2}ms`,
     },
-    });
+  });
 }
 
-export async function action({ request, context }: ActionFunctionArgs)
-{
-  
-  const record = await request.json<InsertPayload>()
+export async function action({ request, context }: ActionFunctionArgs) {
+  const record = await request.json<InsertPayload>();
   console.log({
-   record
+    record,
   });
-    const kv = context.cloudflare.env.kvcache;
-    const db = getDB(context);
-    if (record.type === "INSERT" || record.type === "UPDATE")
-    {
-        const data = await db.select().from(articles);
-        kv.put("articles", JSON.stringify(data));
-    }
-    else if (record.type === "DELETE")
-    {
-      kv.delete("articles");
-      const data = await db.select().from(articles);
-      kv.put("articles", JSON.stringify(data));
-      
-    }
+  const kv = context.cloudflare.env.kvcache;
+  const db = getDB(context);
+  if (record.type === "INSERT" || record.type === "UPDATE") {
+    console.log("insert or update");
+    const data = await db.select().from(articles);
+    kv.put("om2", JSON.stringify(data));
+  } else if (record.type === "DELETE") {
+    console.log("delete");
+    kv.delete("om2");
+    const data = await db.select().from(articles);
+    kv.put("articles", JSON.stringify(data));
+  }
   console.log("webhook success");
   return new Response("OK");
 }
